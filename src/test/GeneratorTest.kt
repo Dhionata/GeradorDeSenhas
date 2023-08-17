b652c8851bc83a7217961833a3244094dc99d79f
Dhionatã <xiyunagi@gmail.com>
2023-08-16 22:49:36 -0300

    Alterado os testes, removido .forEach presente nos testes para diretamente chamar .any ou .all, dependendo do teste, para usar biblioteca padrão do Kotlin. trocado .contains para .matches.

--- src/test/GeneratorTest.kt
+++ src/test/GeneratorTest.kt
@@ -10,33 +10,18 @@ class GeneratorTest {
     @Test
     fun letters() {
         val letters = Generator.letters(n)
-        letters.forEach {
-            if (!it.isLetter()) {
-                Exception("ins't letter: $it")
-            }
-        }
-        Assertions.assertEquals(true, letters.matches("^[a-zA-ZçÇ]+\$".toRegex()))
+        Assertions.assertEquals(true, letters.all { it.isLetter() })
     }
 
     @Test
     fun numbers() {
         val numbers = Generator.numbers(n)
-        numbers.forEach {
-            if (!it.isDigit()) {
-                Exception("ins't digit: $it")
-            }
-        }
-        Assertions.assertEquals(true, numbers.matches("^[0-9]+\$".toRegex()))
+        Assertions.assertEquals(true, numbers.all { it.isDigit() })
     }
 
     @Test
     fun `letters and accents`() {
         val lettersAndAccents = Generator.lettersAndAccents(n)
-        lettersAndAccents.forEach {
-            if (!it.isLetter() && !lettersAndAccents.contains("^[çÇ${Generator.ACCENTS}]+$".toRegex())) {
-                Exception("ins't a letter or accent: $it")
-            }
-        }
         Assertions.assertEquals(
             true, lettersAndAccents.matches("^[a-zA-zçÇ${Generator.ACCENTS}]+\$".toRegex())
         )
@@ -45,48 +30,30 @@ class GeneratorTest {
     @Test
     fun `letters and numbers`() {
         val lettersAndNumbers = Generator.lettersAndNumbers(n)
-        lettersAndNumbers.forEach {
-            if (!it.isLetterOrDigit()) Exception("ins't letter or digit: $it")
-        }
-        Assertions.assertEquals(true, lettersAndNumbers.matches("^[a-zA-ZçÇ0-9]+\$".toRegex()))
+        Assertions.assertEquals(true, lettersAndNumbers.all { it.isLetterOrDigit() })
     }
 
     @Test
     fun `letter, accents and numbers`() {
         val letterAccentsAndNumbers = Generator.letterAccentsAndNumbers(n)
-        letterAccentsAndNumbers.forEach {
-            if (!it.isLetterOrDigit() && letterAccentsAndNumbers.contains("^[${Generator.ACCENTS}]+$".toRegex())) {
-                Exception("ins't letter, accent or number: $it")
-            }
-        }
-        Assertions.assertEquals(
-            true, letterAccentsAndNumbers.matches("^[a-zA-ZçÇ0-9${Generator.ACCENTS}]+\$".toRegex())
-        )
+        Assertions.assertEquals(true,
+            letterAccentsAndNumbers.all { it.isLetterOrDigit() || Generator.ACCENTS.contains(it) })
     }
 
     @Test
     fun `letters, numbers and caracteres`() {
         val lettersNumbersAndCaracteres = Generator.lettersNumbersAndCaracteres(n)
-        lettersNumbersAndCaracteres.forEach {
-            if (!it.isLetterOrDigit() && lettersNumbersAndCaracteres.contains("^[${Generator.SPECIALS}]]+$".toRegex())) {
-                Exception("ins't letter, number and special caracter: $it")
-            }
-        }
-        Assertions.assertEquals(
-            true, lettersNumbersAndCaracteres.matches("^[a-zA-ZçÇ0-9${Generator.SPECIALS}]]+\$".toRegex())
-        )
+        Assertions.assertEquals(true,
+            lettersNumbersAndCaracteres.all {
+                it.isLetterOrDigit() || Generator.SPECIALS.contains(it)
+            })
     }
 
     @Test
     fun unicoded() {
-        Generator.unicodeChars.forEach {
-            if (it.isLetterOrDigit() || it.toString().contains(
-                    "^[çÇ${Generator.SPECIALS}${Generator.ACCENTS}]]+\$".toRegex()
-                )
-            ) {
-                Exception("It's wrong! $it")
-            }
-        }
+        Assertions.assertEquals(false, Generator.unicodeChars.any {
+            it.isLetterOrDigit() || Generator.SPECIALS.contains(it) || Generator.ACCENTS.contains(it)
+        })
     }
 
     @Test
@@ -95,20 +62,8 @@ class GeneratorTest {
 
         repeat(10) {
             val passwordUnicode = generateUnicodeChars(n)
-            passwordUnicode.forEach {
-                if (it.isLetterOrDigit() || passwordUnicode.contains(
-                        "^[${Generator.SPECIALS}${Generator.ACCENTS}]]+$".toRegex()
-                    )
-                ) {
-                    Exception("\nsomething is wrong here!\n $it\n\n")
-                }
-                if (Generator.SPECIALS.contains(it) || Generator.ACCENTS.contains(it)) {
-                    Exception("I found the same: $it")
-                }
-            }
             Assertions.assertEquals(
-                false,
-                passwordUnicode.contains("^[a-zA-Z0-9${Generator.ACCENTS}${Generator.SPECIALS}]]+$".toRegex())
+                false, passwordUnicode.matches("^[a-zA-Z0-9${Generator.ACCENTS}${Generator.SPECIALS}]]+$".toRegex())
             )
         }
     }
