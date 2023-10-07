c960e0480a9f8a5d97fdf0bbea8ac2bbcfc53a5e
Dhionatã <xiyunagi@gmail.com>
2023-10-07 01:55:15 -0300

    Adicionado número total de caracteres com sumOf{}. Imports agora de ACCENTS, SPECIALS e unicodeChars. Adicionado também @RepeatedTest(5) para `distribution, average and unique characters`().

--- src/test/GeneratorTest.kt
+++ src/test/GeneratorTest.kt
@@ -1,8 +1,12 @@
 package test
 
 import Generator
+import Generator.ACCENTS
+import Generator.SPECIALS
 import Generator.allMixed
+import Generator.unicodeChars
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 
 class GeneratorTest {
@@ -23,8 +27,7 @@ class GeneratorTest {
     @Test
     fun `letters and accents`() {
         val lettersAndAccents = Generator.lettersAndAccents(n)
-        Assertions.assertTrue(lettersAndAccents.all { it.isLetter() || Generator.ACCENTS.contains(it) }
-        )
+        Assertions.assertTrue(lettersAndAccents.all { it.isLetter() || ACCENTS.contains(it) })
     }
 
     @Test
@@ -36,44 +39,49 @@ class GeneratorTest {
     @Test
     fun `letter, numbers and accents`() {
         val letterAccentsAndNumbers = Generator.letterNumbersAndAccents(n)
-        Assertions.assertTrue(
-            letterAccentsAndNumbers.all { it.isLetterOrDigit() || Generator.ACCENTS.contains(it) })
+        Assertions.assertTrue(letterAccentsAndNumbers.all { it.isLetterOrDigit() || ACCENTS.contains(it) })
     }
 
     @Test
     fun `letters, numbers and specials`() {
         val lettersNumbersAndCaracteres = Generator.lettersNumbersAndSpecials(n)
         Assertions.assertTrue(lettersNumbersAndCaracteres.all {
-            it.isLetterOrDigit() || Generator.SPECIALS.contains(it)
+            it.isLetterOrDigit() || SPECIALS.contains(it)
         })
     }
 
     @Test
     fun `letters, numbers, accents and specials`() {
         Assertions.assertFalse(Generator.letterNumbersAccentsAndSpecials(n).any {
-            Generator.unicodeChars.contains(it)
+            unicodeChars.contains(it)
         })
     }
 
     @Test
     fun unicodes() {
-        Assertions.assertFalse(Generator.unicodeChars.any {
-            it.isLetterOrDigit() || Generator.SPECIALS.contains(it) || Generator.ACCENTS.contains(it)
+        Assertions.assertFalse(unicodeChars.any {
+            it.isLetterOrDigit() || SPECIALS.contains(it) || ACCENTS.contains(it)
         })
     }
 
-    @Test
+    @RepeatedTest(5)
     fun `distribution, average and unique characters`() {
         val allMixed = allMixed(n).groupingBy { it }.eachCount().toList().sortedBy { it.second }
+
         allMixed.forEach { (chave, valor) ->
             println(
                 if (chave.isDefined()) "O caractere '$chave' se repete $valor vezes, código Unicode ${
                     String.format(
                         "\\u%04x", chave.code
                     )
-                }" else "\n--Não definido--\nO caractere '$chave' se repete $valor vezes\n--Não definido--\n"
+                }" else "\n--Não definido--\nO caractere '$chave' se repete $valor vezes, código Unicode ${
+                    String.format(
+                        "\\u%04x", chave.code
+                    )
+                }\n"
             )
         }
+
         val biggestDifference =
             allMixed.maxByOrNull { it.second }?.second?.minus(allMixed.minByOrNull { it.second }?.second ?: return)
 
@@ -82,11 +90,11 @@ class GeneratorTest {
         val undefined = allMixed.filter { !it.first.isDefined() }
 
         println(
-            "\n--Dados--\nA média é $average com a maior diferença sendo $biggestDifference\nO número total de " +
-                    "caracteres únicos foi de $unique\n${
+            "\n--Dados--\nA média é $average com a maior diferença sendo $biggestDifference" +
+                    "\nO número total de caracteres únicos foi de $unique\n${
                         if (undefined.isNotEmpty()) "número total de caracteres não definidos é ${undefined.size}"
                         else ""
-                    }"
+                    }\nNúmero total de caracteres: ${allMixed.sumOf { it.second }}"
         )
 
         if (biggestDifference != null) {
