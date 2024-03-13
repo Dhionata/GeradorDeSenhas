2d4a5ea0c0868f63c4e09947b7ba88a52b6b1391
Dhionatã <xiyunagi@gmail.com>
2024-03-13 02:05:26 -0300

    isLetterOrDigit() -> numbers/letters.contains(it), vários caracteres estavam sendo retirados na de forma errônea; Verificação UTF-8 para retirada de alguns caracteres unicode sem representação.

--- src/main/Generator.kt
+++ src/main/Generator.kt
@@ -1,17 +1,20 @@
+package main
+
 object Generator {
     internal val letters = (('a'..'z') + ('A'..'Z'))
     internal val numbers = ('0'..'9').toList()
     internal val accents = "àáâãäèéêëìíîïòóôõöùúûüýÿÁÀÂÃÄÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÝ".toList()
     internal val specials = "!@²#%¨\"&*()-_+/.\$£³¢¬?<>,;:°çÇ~^{}ª[]º'|§´`\\".toList()
-    internal val unicodeChars = CharRange(
-        Char.MIN_VALUE, Char.MAX_VALUE
-    ).filterNot {
-        it.isLetterOrDigit() || specials.contains(it) || accents.contains(it) || it.isWhitespace() || it.isISOControl() || !it.isDefined() || it.isIdentifierIgnorable() || it.isSurrogate()
+    internal val unicodeChars = CharRange(Char.MIN_VALUE, Char.MAX_VALUE).filterNot {
+        val original = it.toString()
+        val bytes = original.toByteArray(Charsets.UTF_8)
+        val decoded = bytes.toString(Charsets.UTF_8)
+        letters.contains(it) || numbers.contains(it) || specials.contains(it) || accents.contains(it) || it.isISOControl() || original != decoded || it.isWhitespace() || !it.isDefined() || it.isIdentifierIgnorable()
     }.toList()
 
     @JvmStatic
     private fun generateRandomString(n: Int, functions: List<List<Char>>): String {
-        val allCharacters = functions.flatten()
+        val allCharacters = functions.flatten().shuffled()
         return buildString {
             repeat(n) { append(allCharacters.random()) }
         }
@@ -35,8 +38,7 @@ object Generator {
     @JvmStatic
     fun lettersNumbersAndSpecials(n: Int): String = generateRandomString(n, listOf(letters, numbers, specials))
 
-    fun letterNumbersAccentsAndSpecials(n: Int): String =
-        generateRandomString(n, listOf(letters, numbers, accents, specials))
+    fun letterNumbersAccentsAndSpecials(n: Int): String = generateRandomString(n, listOf(letters, numbers, accents, specials))
 
     @JvmStatic
     fun allMixed(n: Int): String = generateRandomString(n, listOf(letters, numbers, accents, specials, unicodeChars))
